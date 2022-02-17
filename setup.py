b918fc5de4c8e93f89c9ff82c3a99e09d8a63816
David Hummel <6109326+hummeltech@users.noreply.github.com>
2022-02-17 08:56:36 -0700

    Reformatted files with Black

--- setup.py
+++ setup.py
@@ -2,28 +2,29 @@ from setuptools import setup, find_packages
 
 import openmaps_auth
 
-with open('README.md', 'rt') as fh:
+with open("README.md", "rt") as fh:
     long_description = fh.read()
 
-with open('requirements/base.in', 'rt') as fh:
+with open("requirements/base.in", "rt") as fh:
     install_requires = fh.read().split()
-    
-setup(name='openmaps_auth',
-      version=openmaps_auth.__version__,
-      author='Justin Bronn',
-      author_email='justin.bronn@maxar.com',
-      description='OpenMaps Authentication API',
-      url='https://github.com/radiant-maxar/openmaps-auth',
-      install_requires=install_requires,
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: Web Environment',
-          'Framework :: Django',
-          'Intended Audience :: Developers',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: Apache Software License',
-      ],
+
+setup(
+    name="openmaps_auth",
+    version=openmaps_auth.__version__,
+    author="Justin Bronn",
+    author_email="justin.bronn@maxar.com",
+    description="OpenMaps Authentication API",
+    url="https://github.com/radiant-maxar/openmaps-auth",
+    install_requires=install_requires,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+    ],
 )
