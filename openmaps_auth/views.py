ad045694a9f79f0b0a22c400119d45eda7696691
Justin Bronn <justin.bronn@maxar.com>
2022-02-18 15:09:02 -0700

    Add logging to views and raise 403 when whitelist errors occur (instead of 500).

--- openmaps_auth/views.py
+++ openmaps_auth/views.py
@@ -1,8 +1,20 @@
+import logging
+
+import social_core.exceptions
+import social_django.views
 from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
-from django.http import HttpResponseRedirect, JsonResponse
+from django.core.exceptions import PermissionDenied
+from django.http import (
+    HttpResponseForbidden,
+    HttpResponseRedirect,
+    JsonResponse,
+)
 from django.shortcuts import render
 from django.urls import reverse
+
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -17,6 +29,7 @@ def logout(request):
 
 
 def valid(request):
+    logger.info(request.COOKIES)
     if not request.user.is_authenticated:
         return JsonResponse({}, status=401)
     else:
@@ -33,15 +46,19 @@ def set_osm_cookie(request, response):
 
 
 if settings.OPENMAPS_AUTH_BACKEND:
-    import social_django.views
 
     def callback(request, *args, **kwargs):
-        response = social_django.views.complete(
-            request,
-            settings.OPENMAPS_AUTH_BACKEND,
-            *args,
-            **kwargs,
-        )
+        try:
+            response = social_django.views.complete(
+                request,
+                settings.OPENMAPS_AUTH_BACKEND,
+                *args,
+                **kwargs,
+            )
+        except social_core.exceptions.AuthForbidden as denied:
+            logger.info(denied)
+            raise PermissionDenied
+
         return set_osm_cookie(request, response)
 
     def login(request):
