337035aae75dbb2b096c527dc772fcb03cf7c680
Justin Bronn <justin.bronn@maxar.com>
2023-01-26 17:30:42 -0700

    Explicitly separate legacy/modern TLS download views.

--- openmaps_auth/tls/views.py
+++ openmaps_auth/tls/views.py
@@ -59,34 +59,40 @@ def cert_details(request, pk):
 
 
 @login_required
-def cert_download(request, pk, modern=False):
+def cert_legacy(request, pk):
     cert = get_certificate(request.user, pk)
     with open(cert.p12_file, "rb") as p12_fh:
         p12_data = p12_fh.read()
-    if modern:
-        p12_password = request.user.pkcs12_password.encode("utf-8")
-        p12_obj = pkcs12.load_key_and_certificates(p12_data, p12_password)
-        p12_data = pkcs12.serialize_key_and_certificates(
-            b"",
-            p12_obj[0],
-            p12_obj[1],
-            p12_obj[2],
-            BestAvailableEncryption(p12_password),
-        )
-        logger.info(f"{request.user.email} downloaded modern certificate {cert.pk}")
-    else:
-        logger.info(f"{request.user.email} downloaded certificate {cert.pk}")
+    logger.info(f"{request.user.email} downloaded legacy certificate {cert.pk}")
     return FileResponse(
         io.BytesIO(p12_data),
         as_attachment=True,
         content_type="application/octet-stream",
-        filename=cert.p12_name,
+        filename=cert.p12_name.replace(".p12", ".legacy.p12"),
     )
 
 
 @login_required
 def cert_modern(request, pk):
-    return cert_download(request, pk, modern=True)
+    cert = get_certificate(request.user, pk)
+    with open(cert.p12_file, "rb") as p12_fh:
+        p12_data = p12_fh.read()
+    p12_password = request.user.pkcs12_password.encode("utf-8")
+    p12_obj = pkcs12.load_key_and_certificates(p12_data, p12_password)
+    p12_data = pkcs12.serialize_key_and_certificates(
+        b"",
+        p12_obj[0],
+        p12_obj[1],
+        p12_obj[2],
+        BestAvailableEncryption(p12_password),
+    )
+    logger.info(f"{request.user.email} downloaded modern certificate {cert.pk}")
+    return FileResponse(
+        io.BytesIO(p12_data),
+        as_attachment=True,
+        content_type="application/octet-stream",
+        filename=cert.p12_name.replace(".p12", ".modern.p12"),
+    )
 
 
 @login_required
